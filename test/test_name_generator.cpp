31f74b28f3a13ad855af01606ee45fc5a9d0e1bc
James E. King, III <jking@apache.org>
2017-09-22 17:22:37 -0700

    add RFC-4122 namespaces from appendix C to name generator

--- test/test_name_generator.cpp
+++ test/test_name_generator.cpp
@@ -12,6 +12,7 @@
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/name_generator.hpp>
+#include <boost/uuid/string_generator.hpp>
 #include <boost/detail/lightweight_test.hpp>
 #include <boost/config.hpp>
 
@@ -19,22 +20,31 @@ int main(int, char*[])
 {
     using namespace boost::uuids;
 
-    uuid dns_namespace_uuid = {{0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8}};
+    // Verify well-known uuid namespaces
+    BOOST_TEST_EQ(ns::dns(),
+                  string_generator()("6ba7b810-9dad-11d1-80b4-00c04fd430c8"));
+    BOOST_TEST_EQ(ns::url(),
+                  string_generator()("6ba7b811-9dad-11d1-80b4-00c04fd430c8"));
+    BOOST_TEST_EQ(ns::oid(),
+                  string_generator()("6ba7b812-9dad-11d1-80b4-00c04fd430c8"));
+    BOOST_TEST_EQ(ns::x500dn(),
+                  string_generator()("6ba7b814-9dad-11d1-80b4-00c04fd430c8"));
+
     uuid correct = {{0x21, 0xf7, 0xf8, 0xde, 0x80, 0x51, 0x5b, 0x89, 0x86, 0x80, 0x01, 0x95, 0xef, 0x79, 0x8b, 0x6a}};
     uuid wcorrect = {{0xc3, 0x15, 0x27, 0x0b, 0xa4, 0x66, 0x58, 0x72, 0xac, 0xa4, 0x96, 0x26, 0xce, 0xc0, 0xf4, 0xbe}};
 
-    name_generator gen(dns_namespace_uuid);
+    name_generator gen(ns::dns());
 
     uuid u = gen("www.widgets.com");
-    BOOST_TEST_EQ(u, correct); // we established u == correct previously, this validates the bullet
+    BOOST_TEST_EQ(u, correct);
     BOOST_TEST_EQ(u.variant(), boost::uuids::uuid::variant_rfc_4122);
 
-    // RFC 4122 Section 4.3 Bullet 1, same name in same namespace make the same UUID
+    // RFC 4122 Section 4.3 Bullet 1, same name in same namespace makes the same UUID
     u = gen(std::string("www.widgets.com"));
     BOOST_TEST_EQ(u, correct);
     BOOST_TEST_EQ(u.variant(), boost::uuids::uuid::variant_rfc_4122);
 
-    // RFC 4122 Section 4.3 Bullet 2, two names in the same namespace make a different UUID
+    // RFC 4122 Section 4.3 Bullet 2, two names in the same namespace makes a different UUID
     uuid u2 = gen("www.wonka.com");
     BOOST_TEST_NE(u, u2);
     BOOST_TEST_EQ(u.variant(), boost::uuids::uuid::variant_rfc_4122);
@@ -54,10 +64,15 @@ int main(int, char*[])
     BOOST_TEST_EQ(u, correct);
     BOOST_TEST_EQ(u.variant(), boost::uuids::uuid::variant_rfc_4122);
 
-    // generate the same name in a different namespace, per RFC 4122 Section 4.3 Bullet 3 these must be different UUIDs
-    name_generator other(correct);
+    // RFC 4122 Section 4.3 Bullet 3, same name in different namespaces makes a different UUID
+    name_generator other(ns::url());
     uuid u3 = other("www.widgets.com");
     BOOST_TEST_NE(u, u3);
+
+    // used by documentation
+    uuid udoc = gen("boost.org");
+    std::cout << "boost.org uuid in dns namespace: " << udoc << std::endl;
+    // boost.org uuid in dns namespace: 0043f363-bbb4-5369-840a-322df6ec1926
 
     return boost::report_errors();
 }
