7810839cd97a79e0e528004db670a6a0a5e801fe
James E. King, III <jking@apache.org>
2017-10-31 21:08:54 -0700

    fix build error on cygwin

--- test/test_io.cpp
+++ test/test_io.cpp
@@ -20,17 +20,27 @@
 
 using namespace boost::uuids;
 
-template<class _S, class _T>
-void fail(const _T& in)
+void uufail(const std::string& in)
 {
     uuid out;
-    _S ss;
+    std::stringstream ss;
     ss << in;
     ss >> out;
     BOOST_TEST_EQ(true, ss.fail());
 }
 
-void roundtrip(const uuid& in, const std::string& expect)
+#ifndef BOOST_NO_STD_WSTRING
+void uufail(const std::wstring& in)
+{
+    uuid out;
+    std::wstringstream ss;
+    ss << in;
+    ss >> out;
+    BOOST_TEST_EQ(true, ss.fail());
+}
+#endif
+
+void uuroundtrip(const uuid& in, const std::string& expect)
 {
     std::stringstream ss;
     ss << in;
@@ -46,7 +56,7 @@ void roundtrip(const uuid& in, const std::string& expect)
 }
 
 #ifndef BOOST_NO_STD_WSTRING
-void roundtrip(const uuid& in, const std::wstring& expect)
+void uuroundtrip(const uuid& in, const std::wstring& expect)
 {
     std::wstringstream ss;
     ss << in;
@@ -68,9 +78,9 @@ int main(int, char*[])
     const uuid u2 = {{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}};
     const uuid u3 = {{0x12,0x34,0x56,0x78,0x90,0xab,0xcd,0xef,0x12,0x34,0x56,0x78,0x90,0xab,0xcd,0xef}};
 
-    roundtrip(u1, "00000000-0000-0000-0000-000000000000");
-    roundtrip(u2, "00010203-0405-0607-0809-0a0b0c0d0e0f");
-    roundtrip(u3, "12345678-90ab-cdef-1234-567890abcdef");
+    uuroundtrip(u1, "00000000-0000-0000-0000-000000000000");
+    uuroundtrip(u2, "00010203-0405-0607-0809-0a0b0c0d0e0f");
+    uuroundtrip(u3, "12345678-90ab-cdef-1234-567890abcdef");
 
     std::stringstream ss5;
     ss5 << 'a' << std::right << std::setfill('*') << std::setw(40) << u1 << 'a';
@@ -85,29 +95,29 @@ int main(int, char*[])
     BOOST_TEST_EQ(ss7.str(), "00010203-0405-0607-0809-0a0b0c0d0e0f*********");
 
 #ifndef BOOST_NO_STD_WSTRING
-    roundtrip(u1, L"00000000-0000-0000-0000-000000000000");
-    roundtrip(u2, L"00010203-0405-0607-0809-0a0b0c0d0e0f");
-    roundtrip(u3, L"12345678-90ab-cdef-1234-567890abcdef");
+    uuroundtrip(u1, L"00000000-0000-0000-0000-000000000000");
+    uuroundtrip(u2, L"00010203-0405-0607-0809-0a0b0c0d0e0f");
+    uuroundtrip(u3, L"12345678-90ab-cdef-1234-567890abcdef");
 #endif
 
-    fail<std::stringstream>(std::string("01234567-89ab-cdef-\0123-456789abcdef"));
-    fail<std::stringstream>(std::string("01234567-89ab-cdef-0123456789abcdef"));
-    fail<std::stringstream>(std::string("01234567-89ab-cdef0123-456789abcdef"));
-    fail<std::stringstream>(std::string("01234567-89abcdef-0123-456789abcdef"));
-    fail<std::stringstream>(std::string("0123456789ab-cdef-0123-456789abcdef"));
-    fail<std::stringstream>(std::string("{12345678-90ab-cdef-1234-567890abcdef}"));  // stream is not as flexible as string
-    fail<std::stringstream>(std::string("G1234567-89AB-CDEF-0123-456789abcdef"));
-    fail<std::stringstream>(std::string("01234567-89AB-CDEF-0123-456789abcdeg"));
+    uufail("01234567-89ab-cdef-\0123-456789abcdef");
+    uufail("01234567-89ab-cdef-0123456789abcdef");
+    uufail("01234567-89ab-cdef0123-456789abcdef");
+    uufail("01234567-89abcdef-0123-456789abcdef");
+    uufail("0123456789ab-cdef-0123-456789abcdef");
+    uufail("{12345678-90ab-cdef-1234-567890abcdef}");  // stream is not as flexible as string
+    uufail("G1234567-89AB-CDEF-0123-456789abcdef");
+    uufail("01234567-89AB-CDEF-0123-456789abcdeg");
 
 #ifndef BOOST_NO_STD_WSTRING
-    fail<std::wstringstream>(std::wstring(L"01234567-89ab-cdef-\0123-456789abcdef"));
-    fail<std::wstringstream>(std::wstring(L"01234567-89ab-cdef-0123456789abcdef"));
-    fail<std::wstringstream>(std::wstring(L"01234567-89ab-cdef0123-456789abcdef"));
-    fail<std::wstringstream>(std::wstring(L"01234567-89abcdef-0123-456789abcdef"));
-    fail<std::wstringstream>(std::wstring(L"0123456789ab-cdef-0123-456789abcdef"));
-    fail<std::wstringstream>(std::wstring(L"{12345678-90ab-cdef-1234-567890abcdef}"));  // stream is not as flexible as string
-    fail<std::wstringstream>(std::wstring(L"G1234567-89AB-CDEF-0123-456789abcdef"));
-    fail<std::wstringstream>(std::wstring(L"01234567-89AB-CDEF-0123-456789abcdeg"));
+    uufail(L"01234567-89ab-cdef-\0123-456789abcdef");
+    uufail(L"01234567-89ab-cdef-0123456789abcdef");
+    uufail(L"01234567-89ab-cdef0123-456789abcdef");
+    uufail(L"01234567-89abcdef-0123-456789abcdef");
+    uufail(L"0123456789ab-cdef-0123-456789abcdef");
+    uufail(L"{12345678-90ab-cdef-1234-567890abcdef}");  // stream is not as flexible as string
+    uufail(L"G1234567-89AB-CDEF-0123-456789abcdef");
+    uufail(L"01234567-89AB-CDEF-0123-456789abcdeg");
 #endif
 
     BOOST_TEST(boost::lexical_cast<std::string>(u1) == std::string("00000000-0000-0000-0000-000000000000"));
@@ -134,3 +144,4 @@ int main(int, char*[])
 
     return boost::report_errors();
 }
+
