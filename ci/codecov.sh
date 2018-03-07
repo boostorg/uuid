421aa369ecdafcceb70960c578d539b32622d3e8
James E. King III <jking@apache.org>
2018-03-07 15:35:49 -0500

    improve code coverage results processing

--- ci/codecov.sh
+++ ci/codecov.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# Copyright 2017 James E. King III
+# Copyright 2017, 2018 James E. King III
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
 #      http://www.boost.org/LICENSE_1_0.txt)
@@ -9,7 +9,7 @@
 #
 
 ###
-### NOTE: Make sure you grab and customize .codecov.yml
+### NOTE: Make sure you grab .codecov.yml
 ###
 
 # assumes cwd is the top level directory of the boost project
@@ -21,20 +21,19 @@ B2_VARIANT=debug
 ci/build.sh cxxflags=-fprofile-arcs cxxflags=-ftest-coverage linkflags=-fprofile-arcs linkflags=-ftest-coverage
 
 # switch back to the original source code directory
-# this ensures codecov doesn't get confused
 cd $TRAVIS_BUILD_DIR
 
 # get the version of lcov
 lcov --version
 
 # coverage files are in ../../b2 from this location
-lcov --gcov-tool=gcov-7 --rc lcov_branch_coverage=1 --base-directory `pwd` --directory "$BOOST_ROOT" --capture --output-file all.info
+lcov --gcov-tool=gcov-7 --rc lcov_branch_coverage=1 --base-directory "$BOOST_ROOT/libs/$SELF" --directory "$BOOST_ROOT" --capture --output-file all.info
 
 # all.info contains all the coverage info for all projects - limit to ours
-lcov --gcov-tool=gcov-7 --extract all.info "*/boost/$SELF/*" --output-file coverage.info
+lcov --gcov-tool=gcov-7 --rc lcov_branch_coverage=1 --extract all.info "*/boost/$SELF/*" "*/libs/$SELF/src/*" --output-file coverage.info
 
-# dump a summary just for grins
-lcov --gcov-tool=gcov-7 --list coverage.info
+# dump a summary on the console - helps us identify problems in pathing
+lcov --gcov-tool=gcov-7 --rc lcov_branch_coverage=1 --list coverage.info
 
 #
 # upload to codecov.io
