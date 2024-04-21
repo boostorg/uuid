f506b306b38f74f0bf03ea805956f84dc51196fd
Peter Dimov <pdimov@gmail.com>
2024-04-22 02:43:35 +0300

    Use artisanal implementation of random_device under MinGW, because std::random_device always produces the same sequence

--- include/boost/uuid/detail/random_provider.hpp
+++ include/boost/uuid/detail/random_provider.hpp
@@ -2,10 +2,11 @@
 #define BOOST_UUID_DETAIL_RANDOM_PROVIDER_HPP_INCLUDED
 
 // Copyright (c) 2017 James E. King III
-// Copyright (c) 2024 James E. King III
+// Copyright (c) 2024 Peter Dimov
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#include <boost/uuid/detail/random_device.hpp>
 #include <random>
 #include <memory>
 #include <cstdint>
@@ -18,11 +19,11 @@ class random_provider
 {
 private:
 
-    std::unique_ptr< std::random_device > pdev_;
+    std::unique_ptr< detail::random_device > pdev_;
 
 public:
 
-    random_provider(): pdev_( new std::random_device )
+    random_provider(): pdev_( new detail::random_device )
     {
     }
 
@@ -37,7 +38,7 @@ public:
     template<class Iter>
     void generate(Iter first, Iter last)
     {
-        std::uniform_int_distribution<std::uint32_t> dist( 0, UINT32_MAX );
+        std::uniform_int_distribution<std::uint32_t> dist;
 
         for( ; first != last; ++first )
         {
