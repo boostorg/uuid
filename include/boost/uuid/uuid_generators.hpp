d1e26662d94a502f23cca0de574a780192fce023
Peter Dimov <pdimov@gmail.com>
2024-04-21 16:42:53 +0300

    Add a convenience header boost/uuid.hpp

--- include/boost/uuid/uuid_generators.hpp
+++ include/boost/uuid/uuid_generators.hpp
@@ -13,6 +13,8 @@
 
 #include <boost/uuid/nil_generator.hpp>
 #include <boost/uuid/string_generator.hpp>
+#include <boost/uuid/name_generator_sha1.hpp>
+#include <boost/uuid/name_generator_md5.hpp>
 #include <boost/uuid/name_generator.hpp>
 #include <boost/uuid/random_generator.hpp>
 
