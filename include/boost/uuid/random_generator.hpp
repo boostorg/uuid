5f3850bb1d31620ab7f846ea36675ab8b66ceb0d
Peter Dimov <pdimov@gmail.com>
2024-04-23 02:55:31 +0300

    Move basic_random_generator to basic_random_generator.hpp

--- include/boost/uuid/random_generator.hpp
+++ include/boost/uuid/random_generator.hpp
@@ -9,173 +9,14 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/uuid/detail/random_provider.hpp>
+#include <boost/uuid/basic_random_generator.hpp>
 #include <boost/uuid/uuid.hpp>
-#include <boost/assert.hpp>
-#include <boost/config.hpp>
-#include <type_traits>
-#include <random>
-#include <limits>
+#include <boost/uuid/detail/random_provider.hpp>
 #include <cstring>
 #include <cstdint>
 
 namespace boost {
 namespace uuids {
-
-namespace detail {
-    template<class U>
-    U& set_uuid_random_vv(U& u)
-    {
-        // set variant
-        // must be 0b10xxxxxx
-        *(u.begin() + 8) &= 0xBF;
-        *(u.begin() + 8) |= 0x80;
-
-        // set version
-        // must be 0b0100xxxx
-        *(u.begin() + 6) &= 0x4F; //0b01001111
-        *(u.begin() + 6) |= 0x40; //0b01000000
-
-        return u;
-    }
-}
-
-//! generate a random-based uuid
-//! \param[in]  UniformRandomNumberGenerator  see Boost.Random documentation
-template <typename UniformRandomNumberGenerator>
-class basic_random_generator
-{
-private:
-
-    typedef std::uniform_int_distribution<unsigned long> distribution_type;
-
-    struct impl
-    {
-        UniformRandomNumberGenerator* purng;
-        distribution_type dist;
-
-        explicit impl(UniformRandomNumberGenerator* purng_arg) :
-            purng(purng_arg), dist((std::numeric_limits<unsigned long>::min)(), (std::numeric_limits<unsigned long>::max)())
-        {
-        }
-
-        virtual ~impl() = default;
-
-        impl(impl const&) = delete;
-        impl& operator= (impl const&) = delete;
-    };
-
-    struct urng_holder
-    {
-        UniformRandomNumberGenerator urng;
-    };
-
-#if defined(BOOST_MSVC)
-#pragma warning(push)
-// 'this' : used in base member initializer list
-#pragma warning(disable: 4355)
-#endif
-
-    struct self_contained_impl :
-        public urng_holder,
-        public impl
-    {
-        self_contained_impl() : impl(&this->urng_holder::urng)
-        {
-        }
-    };
-
-#if defined(BOOST_MSVC)
-#pragma warning(pop)
-#endif
-
-public:
-
-    typedef uuid result_type;
-
-    // default constructor creates the random number generator and
-    // if the UniformRandomNumberGenerator is a PseudoRandomNumberGenerator
-    // then it gets seeded by a random_provider.
-    basic_random_generator() : m_impl(new self_contained_impl())
-    {
-        // seed the random number generator if it is capable
-        seed(static_cast< self_contained_impl* >(m_impl)->urng, 0);
-    }
-
-    // keep a reference to a random number generator
-    // don't seed a given random number generator
-    explicit basic_random_generator(UniformRandomNumberGenerator& gen) : m_impl(new impl(&gen))
-    {
-    }
-
-    // keep a pointer to a random number generator
-    // don't seed a given random number generator
-    explicit basic_random_generator(UniformRandomNumberGenerator* gen) : m_impl(new impl(gen))
-    {
-        BOOST_ASSERT(!!gen);
-    }
-
-    basic_random_generator(basic_random_generator&& that) BOOST_NOEXCEPT : m_impl(that.m_impl)
-    {
-        that.m_impl = 0;
-    }
-
-    basic_random_generator& operator= (basic_random_generator&& that) BOOST_NOEXCEPT
-    {
-        if( this != &that )
-        {
-            delete m_impl;
-            m_impl = that.m_impl;
-            that.m_impl = 0;
-        }
-
-        return *this;
-    }
-
-    ~basic_random_generator()
-    {
-        delete m_impl;
-    }
-
-    result_type operator()()
-    {
-        result_type u;
-
-        int i = 0;
-        unsigned long random_value = m_impl->dist( *m_impl->purng );
-        for (uuid::iterator it = u.begin(), end = u.end(); it != end; ++it, ++i) {
-            if (i==sizeof(unsigned long)) {
-                random_value = m_impl->dist( *m_impl->purng );
-                i = 0;
-            }
-
-            // static_cast gets rid of warnings of converting unsigned long to boost::uint8_t
-            *it = static_cast<uuid::value_type>((random_value >> (i*8)) & 0xFF);
-        }
-
-        return detail::set_uuid_random_vv(u);
-    }
-
-private:
-    // Detect whether UniformRandomNumberGenerator has a seed() method which indicates that
-    // it is a PseudoRandomNumberGenerator and needs a seed to initialize it.  This allows
-    // basic_random_generator to take any type of UniformRandomNumberGenerator and still
-    // meet the post-conditions for the default constructor.
-
-    template<class MaybePseudoRandomNumberGenerator, class En = decltype( std::declval<MaybePseudoRandomNumberGenerator&>().seed() )>
-    void seed(MaybePseudoRandomNumberGenerator& rng, int)
-    {
-        detail::random_provider seeder;
-        rng.seed(seeder);
-    }
-
-    template<class MaybePseudoRandomNumberGenerator>
-    void seed(MaybePseudoRandomNumberGenerator&, long)
-    {
-    }
-
-    impl* m_impl;
-};
 
 //! \brief a far less complex random generator that uses
 //!        operating system provided entropy which will
@@ -202,7 +43,17 @@ public:
 
         std::memcpy( result.data, tmp, 16 );
 
-        return detail::set_uuid_random_vv(result);
+        // set variant
+        // must be 0b10xxxxxx
+        *(result.begin() + 8) &= 0xBF;
+        *(result.begin() + 8) |= 0x80;
+
+        // set version
+        // must be 0b0100xxxx
+        *(result.begin() + 6) &= 0x4F; //0b01001111
+        *(result.begin() + 6) |= 0x40; //0b01000000
+
+        return result;
     }
 
 private:
