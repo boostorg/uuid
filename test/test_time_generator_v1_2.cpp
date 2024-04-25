9f5a00e1e282db26beab1b76d3fa9014aacf294f
Peter Dimov <pdimov@gmail.com>
2024-04-25 04:49:04 +0300

    Split test_time_generator_v1 into two

--- test/test_time_generator_v1_2.cpp
+++ test/test_time_generator_v1_2.cpp
@@ -5,57 +5,63 @@
 #include <boost/uuid/time_generator_v1.hpp>
 #include <boost/uuid/uuid_clock.hpp>
 #include <boost/core/lightweight_test.hpp>
-#include <atomic>
 #include <chrono>
-#include <thread>
+#include <atomic>
 #include <set>
-#include <vector>
-#include <cstdint>
-#include <cstring>
 
 using namespace boost::uuids;
 
-uuid::node_type const node{{ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 }};
-
-int const N = 1024;
-
-void threadfunc( std::atomic<time_generator_v1::state_type>& state, std::vector<uuid>& v )
+uuid generate_and_test( time_generator_v1& gen )
 {
-    time_generator_v1 gen( node, state );
+    auto sys_before = std::chrono::time_point_cast<uuid_clock::duration>( std::chrono::system_clock::now() );
 
-    for( int i = 0; i < N; ++i )
-    {
-        v.push_back( gen() );
-    }
+    uuid u = gen();
+
+    BOOST_TEST_EQ( u.variant(), uuid::variant_rfc_4122 );
+    BOOST_TEST_EQ( u.version(), uuid::version_time_based );
+
+    auto sys_after = std::chrono::time_point_cast<uuid_clock::duration>( std::chrono::system_clock::now() );
+
+    auto uuid_time_point = u.time_point_v1();
+
+    auto sys_time_point = uuid_clock::to_sys( uuid_time_point );
+
+    BOOST_TEST( sys_before <= sys_time_point );
+    BOOST_TEST( sys_time_point <= sys_after );
+
+    return u;
 }
 
 int main()
 {
-    int const M = 4;
+    int const N = 1024;
 
-    std::thread th[ M ];
-    std::vector<uuid> v[ M ];
-    
-    std::atomic<time_generator_v1::state_type> state{{ 0, 0 }};
-
-    for( int i = 0; i < M; ++i )
     {
-        th[ i ] = std::thread( threadfunc, std::ref( state ), std::ref( v[ i ] ) );
+        std::set<uuid> set;
+
+        uuid::node_type node{{ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 }};
+        std::atomic< time_generator_v1::state_type > state{{ 0, 0x2222 }};
+
+        time_generator_v1 gen( node, state );
+
+        uuid u1 = generate_and_test( gen );
+
+        BOOST_TEST( u1.node_identifier() == node );
+        BOOST_TEST_EQ( u1.clock_seq(), 0x2222 );
+
+        set.insert( u1 );
+
+        for( int i = 0; i < N; ++i )
+        {
+            uuid u2 = generate_and_test( gen );
+
+            BOOST_TEST( u2.node_identifier() == node );
+
+            set.insert( u2 );
+        }
+
+        BOOST_TEST_EQ( set.size(), N + 1 );
     }
-
-    for( int i = 0; i < M; ++i )
-    {
-        th[ i ].join();
-    }
-
-    std::set<uuid> set;
-
-    for( int i = 0; i < M; ++i )
-    {
-        set.insert( v[ i ].begin(), v[ i ].end() );
-    }
-
-    BOOST_TEST_EQ( set.size(), N * M );
 
     return boost::report_errors();
 }
