9ea6b90879387071966f77e88c766bad67c52184
Peter Dimov <pdimov@gmail.com>
2024-04-30 02:52:04 +0300

    Print libstdc++ version when running tests

---
+++ test/print_config_info.cpp
@@ -0,0 +1,13 @@
+// Copyright 2024 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/config.hpp>
+#include <boost/config/pragma_message.hpp>
+
+BOOST_PRAGMA_MESSAGE( "BOOST_LIBSTDCXX_VERSION=" BOOST_STRINGIZE(BOOST_LIBSTDCXX_VERSION) )
+BOOST_PRAGMA_MESSAGE( "_GLIBCXX_RELEASE=" BOOST_STRINGIZE(_GLIBCXX_RELEASE) )
+
+int main()
+{
+}
