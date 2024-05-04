eb86b7e5e96079c66d1d3e500f9aa6721a064e7d
Peter Dimov <pdimov@gmail.com>
2024-05-04 14:40:53 +0300

    Update test_time_generator_v7_3

--- test/test_time_generator_v7_3.cpp
+++ test/test_time_generator_v7_3.cpp
@@ -38,5 +38,31 @@ int main()
         BOOST_TEST_NE( u1, u2 );
     }
 
+    for( int i = 0; i < N; ++i )
+    {
+        time_generator_v7 gen;
+        time_generator_v7 gen2;
+
+        gen2 = gen;
+
+        uuid u1 = gen();
+        uuid u2 = gen2();
+
+        BOOST_TEST_NE( u1, u2 );
+    }
+
+    for( int i = 0; i < N; ++i )
+    {
+        time_generator_v7 gen;
+        time_generator_v7 gen2;
+
+        gen2 = std::move( gen );
+
+        uuid u1 = gen();
+        uuid u2 = gen2();
+
+        BOOST_TEST_NE( u1, u2 );
+    }
+
     return boost::report_errors();
 }
