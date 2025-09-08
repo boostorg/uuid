5496dd3bbea8b1cda8ab7bb72b56b5826d59849b
Peter Dimov <pdimov@gmail.com>
2025-09-08 21:49:23 +0300

    Add boost/uuid/constants.hpp

---
+++ include/boost/uuid/detail/nil_uuid.hpp
@@ -0,0 +1,20 @@
+#ifndef BOOST_UUID_DETAIL_NIL_UUID_INCLUDED
+#define BOOST_UUID_DETAIL_NIL_UUID_INCLUDED
+
+// Copyright 2025 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/uuid/uuid.hpp>
+
+namespace boost {
+namespace uuids {
+
+inline uuid nil_uuid() noexcept
+{
+    return {{}};
+}
+
+}} // namespace boost::uuids
+
+#endif // #ifndef BOOST_UUID_DETAIL_NIL_UUID_INCLUDED
